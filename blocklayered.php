f8f0c2c0ab2e4b9525a183e9090f71f083c38172
Julien Bourdeau <julien@sigerr.org>
2014-03-27 18:22:40 +0100

    // update version number

--- blocklayered.php
+++ blocklayered.php
@@ -38,7 +38,7 @@ class BlockLayered extends Module
 	{
 		$this->name = 'blocklayered';
 		$this->tab = 'front_office_features';
-		$this->version = '1.10.6';
+		$this->version = '1.10.7';
 		$this->author = 'PrestaShop';
 		$this->need_instance = 0;
 		$this->bootstrap = true;
