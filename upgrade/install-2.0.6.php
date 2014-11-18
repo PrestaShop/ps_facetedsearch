92d4e63b9f1ece3e75a304941c49c072451186a2
gRoussac <gregory.roussac@prestashop.com>
2014-11-18 15:03:54 +0100

    // version issue

--- upgrade/install-2.0.6.php
+++ upgrade/install-2.0.6.php
@@ -3,7 +3,7 @@
 if (!defined('_PS_VERSION_'))
 	exit;
 
-function upgrade_module_2_0_5($object)
+function upgrade_module_2_0_6($object)
 {
 	$return = Configuration::updateValue('PS_LAYERED_FILTER_PRICE_ROUNDING', 1);
 	$query = 'ALTER TABLE `'._DB_PREFIX_.'layered_indexable_attribute_group_lang_value` CHANGE `url_name` `url_name` VARCHAR( 128 ) NULL DEFAULT NULL , CHANGE `meta_title` `meta_title` VARCHAR( 128 ) NULL DEFAULT NULL';
