edb56d247ef56640a8a8aef023b406733e296189
joce <jocelyn.Fournier@gmail.com>
2015-07-02 09:20:25 +0200

    [-] Avoid failure during upgrade

--- upgrade/install-2.0.11.php
+++ upgrade/install-2.0.11.php
@@ -5,8 +5,17 @@ if (!defined('_PS_VERSION_'))
 
 function upgrade_module_2_0_11($object)
 {
-	$query = 'ALTER TABLE `'._DB_PREFIX_.'layered_product_attribute` DROP PRIMARY KEY, ADD PRIMARY KEY (`id_attribute`, `id_product`, `id_shop`), DROP KEY `id_attribute_group`, ADD UNIQUE KEY `id_attribute_group` (`id_attribute_group`,`id_attribute`,`id_product`,`id_shop`)';
+	$query = 'ALTER TABLE `'._DB_PREFIX_.'layered_product_attribute` DROP PRIMARY KEY';
 	$return = Db::getInstance()->execute($query);
+
+	$query = 'ALTER TABLE `'._DB_PREFIX_.'layered_product_attribute` ADD PRIMARY KEY (`id_attribute`, `id_product`, `id_shop`)';
+	$return &= Db::getInstance()->execute($query);
+
+	$query = 'ALTER TABLE `'._DB_PREFIX_.'layered_product_attribute` DROP KEY `id_attribute_group`';
+	$return &= Db::getInstance()->execute($query);
+
+	$query = 'ALTER TABLE `'._DB_PREFIX_.'layered_product_attribute` ADD UNIQUE KEY `id_attribute_group` (`id_attribute_group`,`id_attribute`,`id_product`,`id_shop`)';
+	$return &= Db::getInstance()->execute($query);
 
 	return $return;
 }
