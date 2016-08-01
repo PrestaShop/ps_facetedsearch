6853b389f41ea08d9fd1e69d47b2a91613e7d5d6
Julien Bourdeau <julien@sigerr.org>
2016-08-01 17:45:07 +0200

    CO: Add backtick surrounding order_clause variable

--- ps_facetedsearch.php
+++ ps_facetedsearch.php
@@ -1516,7 +1516,7 @@ class Ps_Facetedsearch extends Module
                     LEFT JOIN '._DB_PREFIX_.'manufacturer m ON (m.id_manufacturer = p.id_manufacturer)
                     '.Product::sqlStock('p', 0).'
                     WHERE '.$alias_where.'.`active` = 1 AND '.$alias_where.'.`visibility` IN ("both", "catalog")
-                    ORDER BY '.bqSQL($order_clause).' , cp.id_product'.
+                    ORDER BY `'.bqSQL($order_clause).'` , cp.id_product'.
                     ' LIMIT '.(((int)$page - 1) * $products_per_page.','.$products_per_page), true, false);
             } else {
                 $products = Db::getInstance()->executeS('
@@ -1545,7 +1545,7 @@ class Ps_Facetedsearch extends Module
                     '.Product::sqlStock('p', 0).'
                     WHERE '.$alias_where.'.`active` = 1 AND '.$alias_where.'.`visibility` IN ("both", "catalog")
                     GROUP BY product_shop.id_product
-                    ORDER BY '.bqSQL($order_clause).' , cp.id_product'.
+                    ORDER BY `'.bqSQL($order_clause).'` , cp.id_product'.
                     ' LIMIT '.(((int)$page - 1) * $products_per_page.','.$products_per_page), true, false);
             }
         }
