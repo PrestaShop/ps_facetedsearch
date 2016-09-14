bb60776f1279ab76d6bd60df211827b670dbc8e0
Maxime Biloé <maxime.biloe@prestashop.com>
2016-09-14 11:14:13 +0200

    // Version updated to v1.0.6

--- ps_facetedsearch.php
+++ ps_facetedsearch.php
@@ -45,7 +45,7 @@ class Ps_Facetedsearch extends Module
     {
         $this->name = 'ps_facetedsearch';
         $this->tab = 'front_office_features';
-        $this->version = '1.0.5';
+        $this->version = '1.0.6';
         $this->author = 'PrestaShop';
         $this->need_instance = 0;
         $this->bootstrap = true;
