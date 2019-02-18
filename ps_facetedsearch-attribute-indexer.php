099c9de111cc7b94ffa464ae6156f2d1b64e3962
Pierre RAMBAUD <pierre.rambaud86@gmail.com>
2019-02-18 10:59:33 +0100

    Use __DIR__ instead of dirname(__FILE__)

--- ps_facetedsearch-attribute-indexer.php
+++ ps_facetedsearch-attribute-indexer.php
@@ -1,7 +1,7 @@
 <?php
 
-include dirname(__FILE__) . '/../../config/config.inc.php';
-include dirname(__FILE__) . '/ps_facetedsearch.php';
+include __DIR__ . '/../../config/config.inc.php';
+include __DIR__ . '/ps_facetedsearch.php';
 
 if (substr(Tools::encrypt('ps_facetedsearch/index'), 0, 10) != Tools::getValue('token') || !Module::isInstalled('ps_facetedsearch')) {
     die('Bad token');
