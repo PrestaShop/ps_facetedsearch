e20c2bafd80498bac5e986ce7e4aeec83dcc3264
Thomas Nabord <thomas.nabord@prestashop.com>
2019-04-16 10:50:52 +0100

    Separate stages for JS & PHP tests

--- tests/php/phpstan/bootstrap.php
+++ tests/php/phpstan/bootstrap.php
@@ -1,24 +1,30 @@
 <?php
+$rootDir = getenv('_PS_ROOT_DIR_');
+
+if (!$rootDir) {
+    echo '[ERROR] Define _PS_ROOT_DIR_ with the path to PrestaShop folder'. PHP_EOL;
+    exit(1);
+}
 
 // Add module composer autoloader
-require_once dirname(__DIR__) . '/../../vendor/autoload.php';
-
+require_once dirname(__DIR__) . "/../../vendor/autoload.php";
 // Add PrestaShop composer autoload
-define('_PS_ADMIN_DIR_', '/web/PrestaShop/admin-dev/');
+define('_PS_ADMIN_DIR_', $rootDir . '/admin-dev/');
 define('PS_ADMIN_DIR', _PS_ADMIN_DIR_);
-require_once '/web/PrestaShop/config/defines.inc.php';
-require_once '/web/PrestaShop/config/autoload.php';
-require_once '/web/PrestaShop/config/bootstrap.php';
+require_once $rootDir . "/config/defines.inc.php";
+require_once $rootDir . "/config/autoload.php";
+require_once $rootDir . "/config/bootstrap.php";
 
 // Make sure loader php-parser is coming from php stan composer
 $loader = new \Composer\Autoload\ClassLoader();
-$loader->setPsr4('PhpParser\\', ['/composer/vendor/nikic/php-parser/lib/PhpParser']);
+$loader->setPsr4('PhpParser\\', array('/composer/vendor/nikic/php-parser/lib/PhpParser'));
 $loader->register(true);
 
 // We must declare these constant in this boostrap script.
 // Ignoring the error partern with this value will throw another error if not found
 // during the checks.
 $constantsToDefine = [
+  '_DB_PREFIX_',
   '_PS_SSL_PORT_',
   '_THEME_NAME_',
   '_PARENT_THEME_NAME_',
