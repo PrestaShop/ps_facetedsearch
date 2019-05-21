f45ce4c4a9163e9ba11a9afa012bbb1736232d60
Pierre RAMBAUD <pierre.rambaud86@gmail.com>
2019-05-21 18:04:21 +0200

    Bump to v3.0.1

--- ps_facetedsearch.php
+++ ps_facetedsearch.php
@@ -84,7 +84,7 @@ class Ps_Facetedsearch extends Module
     {
         $this->name = 'ps_facetedsearch';
         $this->tab = 'front_office_features';
-        $this->version = '3.0.0';
+        $this->version = '3.0.1';
         $this->author = 'PrestaShop';
         $this->need_instance = 0;
         $this->bootstrap = true;
