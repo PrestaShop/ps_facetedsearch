84b44664a8a1974b9f9f3c59e689abc5b0b8b7c1
Pierre RAMBAUD <pierre.rambaud86@gmail.com>
2019-05-23 16:36:03 +0200

    Only rebuild table while upgrading to prevent a fatal error with memory

--- ps_facetedsearch.php
+++ ps_facetedsearch.php
@@ -192,7 +192,7 @@ class Ps_Facetedsearch extends Module implements WidgetInterface
             $this->rebuildLayeredCache();
         }
 
-        $this->installPriceIndexTable();
+        $this->rebuildPriceIndexTable();
         $this->installIndexableAttributeTable();
         $this->installProductAttributeTable();
 
@@ -278,7 +278,7 @@ class Ps_Facetedsearch extends Module implements WidgetInterface
     public function fullPricesIndexProcess($cursor = 0, $ajax = false, $smart = false)
     {
         if ($cursor == 0 && !$smart) {
-            $this->installPriceIndexTable();
+            $this->rebuildPriceIndexTable();
         }
 
         return $this->indexPrices($cursor, true, $ajax, $smart);
@@ -1162,27 +1162,9 @@ VALUES(' . $last_id . ', ' . (int) $idShop . ')');
     }
 
     /**
-     * create table product attribute.
-     */
-    private function installProductAttributeTable()
-    {
-        $this->getDatabase()->execute('DROP TABLE IF EXISTS `' . _DB_PREFIX_ . 'layered_product_attribute`');
-        $this->getDatabase()->execute(
-            'CREATE TABLE `' . _DB_PREFIX_ . 'layered_product_attribute` (
-            `id_attribute` int(10) unsigned NOT NULL,
-            `id_product` int(10) unsigned NOT NULL,
-            `id_attribute_group` int(10) unsigned NOT NULL DEFAULT "0",
-            `id_shop` int(10) unsigned NOT NULL DEFAULT "1",
-            PRIMARY KEY (`id_attribute`, `id_product`, `id_shop`),
-            UNIQUE KEY `id_attribute_group` (`id_attribute_group`,`id_attribute`,`id_product`, `id_shop`)
-            ) ENGINE=' . _MYSQL_ENGINE_ . ' DEFAULT CHARSET=utf8;'
-        );
-    }
-
-    /**
      * Install price indexes table
      */
-    private function installPriceIndexTable()
+    public function rebuildPriceIndexTable()
     {
         $this->getDatabase()->execute('DROP TABLE IF EXISTS `' . _DB_PREFIX_ . 'layered_price_index`');
 
@@ -1198,6 +1180,24 @@ VALUES(' . $last_id . ', ' . (int) $idShop . ')');
             INDEX `id_currency` (`id_currency`),
             INDEX `price_min` (`price_min`),
             INDEX `price_max` (`price_max`)
+            ) ENGINE=' . _MYSQL_ENGINE_ . ' DEFAULT CHARSET=utf8;'
+        );
+    }
+
+    /**
+     * create table product attribute.
+     */
+    private function installProductAttributeTable()
+    {
+        $this->getDatabase()->execute('DROP TABLE IF EXISTS `' . _DB_PREFIX_ . 'layered_product_attribute`');
+        $this->getDatabase()->execute(
+            'CREATE TABLE `' . _DB_PREFIX_ . 'layered_product_attribute` (
+            `id_attribute` int(10) unsigned NOT NULL,
+            `id_product` int(10) unsigned NOT NULL,
+            `id_attribute_group` int(10) unsigned NOT NULL DEFAULT "0",
+            `id_shop` int(10) unsigned NOT NULL DEFAULT "1",
+            PRIMARY KEY (`id_attribute`, `id_product`, `id_shop`),
+            UNIQUE KEY `id_attribute_group` (`id_attribute_group`,`id_attribute`,`id_product`, `id_shop`)
             ) ENGINE=' . _MYSQL_ENGINE_ . ' DEFAULT CHARSET=utf8;'
         );
     }
