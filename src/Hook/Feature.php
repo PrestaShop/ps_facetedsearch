f240786bb40286fa8169bfa9803947873531c15f
Julius Zukauskas <zuk3975@gmail.com>
2019-09-05 17:57:57 +0300

    Modify features form and its saving

--- src/Hook/Feature.php
+++ src/Hook/Feature.php
@@ -26,17 +26,74 @@
 
 namespace PrestaShop\Module\FacetedSearch\Hook;
 
+use PrestaShop\Module\FacetedSearch\Form\Feature\FormDataProvider;
+use PrestaShop\Module\FacetedSearch\Form\Feature\FormModifier;
+use PrestaShopDatabaseException;
+use Ps_Facetedsearch;
 use Tools;
 use Language;
 
 class Feature extends AbstractHook
 {
+    /**
+     * @var FormModifier $formModifier
+     */
+    private $formModifier;
+
+    /**
+     * @var FormDataProvider
+     */
+    private $dataProvider;
+
+    public function __construct(Ps_Facetedsearch $module)
+    {
+        parent::__construct($module);
+
+        $this->formModifier = new FormModifier($module->getContext());
+        $this->dataProvider = new FormDataProvider($module->getDatabase());
+    }
+
     const AVAILABLE_HOOKS = [
         'actionFeatureSave',
         'actionFeatureDelete',
         'displayFeatureForm',
         'displayFeaturePostProcess',
+        'actionAfterCreateFeatureFormHandler',
+        'actionAfterUpdateFeatureFormHandler',
     ];
+
+    /**
+     * Hook for modifying feature form formBuilder
+     *
+     * @param array $params
+     *
+     * @throws PrestaShopDatabaseException
+     */
+    public function actionFeatureFormBuilderModifier(array $params)
+    {
+        $this->formModifier->modify($params['form_builder'], $this->dataProvider->getData($params));
+
+    }
+
+    /**
+     * Hook after create feature. @since ps v1.7.7
+     *
+     * @param array $params
+     */
+    public function actionAfterCreateFeatureFormHandler(array $params)
+    {
+        $this->save($params);
+    }
+
+    /**
+     * Hook after update feature. @since ps v1.7.7
+     *
+     * @param array $params
+     */
+    public function actionAfterUpdateFeatureFormHandler(array $params)
+    {
+        $this->save($params);
+    }
 
     /**
      * Hook after delete a feature
@@ -145,6 +202,59 @@ class Feature extends AbstractHook
                 \'' . pSQL(Tools::link_rewrite($seoUrl)) . '\',
                 \'' . pSQL($metaTitle, true) . '\')'
             );
+        }
+
+        $this->module->invalidateLayeredFilterBlockCache();
+    }
+
+    /**
+     * Saves feature form. @since ps 1.7.7
+     *
+     * @param array $params
+     */
+    private function save(array $params)
+    {
+        $featureId = (int) $params['id'];
+
+        $this->database->execute(
+            'DELETE FROM ' . _DB_PREFIX_ . 'layered_indexable_feature
+            WHERE `id_feature` = ' . $featureId
+        );
+        $this->database->execute(
+            'DELETE FROM ' . _DB_PREFIX_ . 'layered_indexable_feature_lang_value
+            WHERE `id_feature` = ' . $featureId
+        );
+
+        $this->database->execute(
+            'INSERT INTO ' . _DB_PREFIX_ . 'layered_indexable_feature
+            (`id_feature`, `indexable`)
+            VALUES (' . $featureId . ', ' . (int) $params['form_data']['layered_indexable'] . ')'
+        );
+
+        $defaultLangId = (int) Configuration::get('PS_LANG_DEFAULT');
+        foreach (Language::getLanguages(false) as $language) {
+            $langId = (int) $language['id_lang'];
+            $metaTitle = pSQL($params['form_data']['meta_title'][$langId]);
+            $seoUrl = $params['form_data']['url_name'][$langId];
+            $name = $params['form_data']['name'][$langId] ?: $params['form_data']['name'][$defaultLangId];
+
+            if (empty($seoUrl)) {
+                $seoUrl = $name;
+            }
+
+            $tableName = _DB_PREFIX_ . 'layered_indexable_feature_lang_value';
+            $url = pSQL(Tools::link_rewrite($seoUrl));
+
+            $query = sprintf("
+              INSERT INTO $tableName(`id_feature`, `id_lang`, `url_name`, `meta_title`)
+              VALUES (%s, %s, '%s', '%s')",
+                $featureId,
+                $langId,
+                $url,
+                $metaTitle
+            );
+
+            $this->database->execute($query);
         }
 
         $this->module->invalidateLayeredFilterBlockCache();
