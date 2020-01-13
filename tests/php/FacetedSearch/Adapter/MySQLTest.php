d98295c3ce886219b5b942c7895be6af33cd6ad8
Edvinas Gurevicius <edvinas.gure@gmail.com>
2020-01-13 23:13:06 +0200

    testing showLastEnabled improved

--- tests/php/FacetedSearch/Adapter/MySQLTest.php
+++ tests/php/FacetedSearch/Adapter/MySQLTest.php
@@ -28,6 +28,7 @@ namespace PrestaShop\Module\FacetedSearch\Tests\Adapter;
 
 use stdClass;
 use Db;
+use Product;
 use Context;
 use Configuration;
 use StockAvailable;
@@ -420,13 +421,18 @@ class MySQLTest extends MockeryTestCase
             ->andReturn(true);
         Configuration::setStaticExpectations($configurationMock);
 
+        $productMock = Mockery::namedMock(Product::class);
+        $productMock->shouldReceive('isAvailableWhenOutOfStock')
+            ->with(2)
+            ->andReturn(true);
+
         $this->adapter->addSelectField('id_product');
         $this->adapter->useFiltersAsInitialPopulation();
         $this->adapter->setOrderField('position');
         $this->adapter->setOrderDirection('desc');
 
         $this->assertEquals(
-            'SELECT p.id_product, sa.out_of_stock FROM (SELECT p.id_product, p.id_manufacturer, sa.quantity, p.condition, p.weight, p.price, cp.position FROM ps_product p LEFT JOIN ps_product_attribute pa ON (p.id_product = pa.id_product) LEFT JOIN ps_product_attribute_combination pac ON (pa.id_product_attribute = pac.id_product_attribute) LEFT JOIN ps_stock_available sa ON (p.id_product = sa.id_product AND IFNULL(pac.id_product_attribute, 0) = sa.id_product_attribute) INNER JOIN ps_category_product cp ON (p.id_product = cp.id_product)) p LEFT JOIN ps_product_attribute pa ON (p.id_product = pa.id_product) LEFT JOIN ps_product_attribute_combination pac ON (pa.id_product_attribute = pac.id_product_attribute) LEFT JOIN ps_stock_available sa ON (p.id_product = sa.id_product AND IFNULL(pac.id_product_attribute, 0) = sa.id_product_attribute) INNER JOIN ps_category_product cp ON (p.id_product = cp.id_product) ORDER BY p.quantity <= 0, FIELD(sa.out_of_stock, 2, 1, 0) ASC, p.position DESC',
+            'SELECT p.id_product, sa.out_of_stock FROM (SELECT p.id_product, p.id_manufacturer, sa.quantity, p.condition, p.weight, p.price, cp.position FROM ps_product p LEFT JOIN ps_product_attribute pa ON (p.id_product = pa.id_product) LEFT JOIN ps_product_attribute_combination pac ON (pa.id_product_attribute = pac.id_product_attribute) LEFT JOIN ps_stock_available sa ON (p.id_product = sa.id_product AND IFNULL(pac.id_product_attribute, 0) = sa.id_product_attribute) INNER JOIN ps_category_product cp ON (p.id_product = cp.id_product)) p LEFT JOIN ps_product_attribute pa ON (p.id_product = pa.id_product) LEFT JOIN ps_product_attribute_combination pac ON (pa.id_product_attribute = pac.id_product_attribute) LEFT JOIN ps_stock_available sa ON (p.id_product = sa.id_product AND IFNULL(pac.id_product_attribute, 0) = sa.id_product_attribute) INNER JOIN ps_category_product cp ON (p.id_product = cp.id_product) ORDER BY p.quantity <= 0, FIELD(sa.out_of_stock, 0) ASC, p.position DESC',
             $this->adapter->getQuery()
         );
     }
