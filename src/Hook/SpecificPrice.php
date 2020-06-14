11674852f0ecc98d0ce7ac9302398e4a13d2ad59
matks <mathieu.ferment@prestashop.com>
2020-06-14 15:04:22 +0200

    Fix license headers

--- src/Hook/SpecificPrice.php
+++ src/Hook/SpecificPrice.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * 2007-2019 PrestaShop.
+ * 2007-2020 PrestaShop and Contributors
  *
  * NOTICE OF LICENSE
  *
@@ -12,14 +12,8 @@
  * obtain it through the world-wide-web, please send an email
  * to license@prestashop.com so we can send you a copy immediately.
  *
- * DISCLAIMER
- *
- * Do not edit or add to this file if you wish to upgrade PrestaShop to newer
- * versions in the future. If you wish to customize PrestaShop for your
- * needs please refer to http://www.prestashop.com for more information.
- *
  * @author    PrestaShop SA <contact@prestashop.com>
- * @copyright 2007-2019 PrestaShop SA
+ * @copyright 2007-2020 PrestaShop SA and Contributors
  * @license   https://opensource.org/licenses/AFL-3.0 Academic Free License 3.0 (AFL-3.0)
  * International Registered Trademark & Property of PrestaShop SA
  */
