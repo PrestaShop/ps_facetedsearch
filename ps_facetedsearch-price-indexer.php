3026803b9372bbab52d84092a928b1cd136b7f43
Pierre RAMBAUD <pierre.rambaud86@gmail.com>
2020-07-08 14:40:25 +0200

    Remove useless part of code

--- ps_facetedsearch-price-indexer.php
+++ ps_facetedsearch-price-indexer.php
@@ -24,23 +24,6 @@ if (substr(Tools::encrypt('ps_facetedsearch/index'), 0, 10) != Tools::getValue('
     die('Bad token');
 }
 
-if (!Tools::getValue('ajax')) {
-    // Case of nothing to do but showing a message (1)
-    if (Tools::getValue('return_message') !== false) {
-        echo '1';
-        die();
-    }
-
-    if (Tools::usingSecureMode()) {
-        $domain = Tools::getShopDomainSsl(true);
-    } else {
-        $domain = Tools::getShopDomain(true);
-    }
-    // Return a content without waiting the end of index execution
-    header('Location: ' . $domain . __PS_BASE_URI__ . 'modules/ps_facetedsearch/ps_facetedsearch-price-indexer.php?token=' . Tools::getValue('token') . '&return_message=' . (int) Tools::getValue('cursor'));
-    flush();
-}
-
 Shop::setContext(Shop::CONTEXT_ALL);
 
 $module = new Ps_Facetedsearch();
