df81ff7d4607e4303db25b75a7dc11514180d655
ProTreo <worldman1994@gmail.com>
2020-11-30 17:33:16 +0300

    Revert "fixed webpack config"
    This reverts commit 3c9e804b5366e1f14425653fffb7e62c803c9a82.

--- webpack.config.js
+++ webpack.config.js
@@ -40,6 +40,7 @@ module.exports = {
         test: /\.scss$/,
         use: [
           'style-loader',
+          MiniCssExtractPlugin.loader,
           'css-loader',
           'sass-loader',
         ],
