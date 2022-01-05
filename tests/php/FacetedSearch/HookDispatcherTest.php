6224694bfc6e230b3a322f4a3b33de2dc071bb18
Daniel Hlavacek <daniel.hlavacek@hotmail.cz>
2022-01-05 13:22:39 +0100

    Update test expected hook count

--- tests/php/FacetedSearch/HookDispatcherTest.php
+++ tests/php/FacetedSearch/HookDispatcherTest.php
@@ -47,7 +47,7 @@ class HookDispatcherTest extends MockeryTestCase
 
     public function testGetAvailableHooks()
     {
-        $this->assertCount(27, $this->dispatcher->getAvailableHooks());
+        $this->assertCount(28, $this->dispatcher->getAvailableHooks());
         $this->assertEquals(
             [
                 'actionAttributeGroupDelete',
