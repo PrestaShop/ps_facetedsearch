0286927fb73fc831a78dbfc5446cea9ee98f1255
Jonathan Lelievre <jonathan.lelievre@prestashop.com>
2024-02-14 15:41:57 +0100

    Deprecate all standalone endpoints

--- ps_facetedsearch-clear-cache.php
+++ ps_facetedsearch-clear-cache.php
@@ -17,6 +17,14 @@
  * @copyright Since 2007 PrestaShop SA and Contributors
  * @license   https://opensource.org/licenses/AFL-3.0 Academic Free License 3.0 (AFL-3.0)
  */
+
+/*
+ * This standalone endpoint is deprecated, it should not be used anymore and should be removed along with the
+ * htaccess file that still allows it to work despite the security policy from the core forbidding this kind
+ * of file to be executed.
+ */
+@trigger_error('This endpoint has been deprecated and will be removed in the next major version for this module, you should rely on Ps_FacetedSearchCronModuleFrontController instead.', E_USER_DEPRECATED);
+
 require_once __DIR__ . '/../../config/config.inc.php';
 require_once __DIR__ . '/ps_facetedsearch.php';
 
