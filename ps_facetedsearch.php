a0952b691f1fbe0bf45b4a10ffb492daab499270
mattgoud <matthias.goudjil@prestashop.com>
2026-09-08 17:09:17 +0200

    Bump version to 5.1.0

--- ps_facetedsearch.php
+++ ps_facetedsearch.php
@@ -97,7 +97,7 @@ class Ps_Facetedsearch extends Module implements WidgetInterface
     {
         $this->name = 'ps_facetedsearch';
         $this->tab = 'front_office_features';
-        $this->version = '5.0.0';
+        $this->version = '5.1.0';
         $this->author = 'PrestaShop';
         $this->need_instance = 0;
         $this->bootstrap = true;
